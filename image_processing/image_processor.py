a60de3e1579fe30351c507fb043b9ee76d574f26
SVladkov <s.v.vladkov@gmail.com>
2014-06-26 22:51:23 +0300

    Transparent background of the coin image

--- image_processing/image_processor.py
+++ image_processing/image_processor.py
@@ -1,5 +1,7 @@
 from PIL import ImageFilter
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageOps
 
 ext = ".jpg"
 
@@ -63,7 +65,7 @@ class ImageProcessor():
 				in_sequence = False
 				number_of_consecutive = 0
 				
-			column -= 1
+			column -= 2 
 		
 	def find_left_border(self):
 		column = 1
@@ -78,7 +80,7 @@ class ImageProcessor():
 		while column < self.width:
 			pixel_column_difference = self.find_column_difference(column, -1)
 			for k in range(len(pixel_column_difference)):
-				if pixel_column_difference[k] > 50:
+				if pixel_column_difference[k] > 60:
 					has_big_difference = True
 					break
 				else:
@@ -96,7 +98,7 @@ class ImageProcessor():
 				in_sequence = False		
 				number_of_consecutive = 0					
 					
-			column += 1
+			column += 2
 			
 	def find_upper_border(self):
 		row = 1
@@ -111,7 +113,7 @@ class ImageProcessor():
 		while row < self.height:
 			pixel_row_difference = self.find_row_difference(row, -1)
 			for k in range(len(pixel_row_difference)):
-				if pixel_row_difference[k] > 50:
+				if pixel_row_difference[k] > 60:
 					has_big_difference = True
 					break
 				else:
@@ -129,7 +131,7 @@ class ImageProcessor():
 				in_sequence = False
 				number_of_consecutive = 0
 				
-			row += 1
+			row += 2
 		
 	def find_lower_border(self):
 		row = self.height - 2
@@ -162,7 +164,7 @@ class ImageProcessor():
 				in_sequence = False
 				number_of_consecutive = 0
 				
-			row -= 1
+			row -= 2
 		
 	def find_row_difference(self, row, row_compare):
 		counter = 0
@@ -200,24 +202,30 @@ class ImageProcessor():
 			
 		return pixel_column_difference
 		
-	'''def cut_coin(self):
-		region = self.crop()
-		region.show()'''
+	def make_coin_image(self):
+		self.crop()
+		cropped_coin = self.cropped
+		cropped_coin.convert('RGBA')
+			
+		pixels_of_cropped = cropped_coin.load()
+		middle_x = (self.right - self.left) / 2
+		middle_y = (self.lower - self.upper) / 2
+		radius = middle_x - 1
+		
+		#ImageProcessor.draw_circle(pixels_of_cropped, middle_x, middle_y, radius)
+		
+		self.make_background_transparent(cropped_coin)
+		
+		cropped_coin.save("cropped_coin.png")
 		
 	def crop(self):
-		left = self.find_left_border()
-		right = self.find_right_border()
-		upper = self.find_upper_border()
-		lower = self.find_lower_border()
+		self.left = self.find_left_border()
+		self.right = self.find_right_border()
+		self.upper = self.find_upper_border()
+		self.lower = self.find_lower_border()
 		
-		box = (left, upper, right, lower)
-		cropped = self.image.crop(box)
-			
-		pixels_of_cropped = cropped.load()
-		ImageProcessor.draw_circle(pixels_of_cropped, (right - left) / 2, (lower - upper) / 2, (right - left) / 2 - 1)
-		
-		cropped.show()
-		return cropped
+		box = (self.left, self.upper, self.right, self.lower)
+		self.cropped = self.image.crop(box)		
 		
 	@staticmethod
 	def draw_circle(pixels, center_x, center_y, radius):
@@ -225,15 +233,15 @@ class ImageProcessor():
 		y = 0
 		radius_error = 1 - x
 		
-		while x >= y:
-			pixels[center_x + x, center_y + y] = (0, 0, 0)
-			pixels[center_x + y, center_y + x] = (0, 0, 0)
-			pixels[center_x - x, center_y + y] = (0, 0, 0)
-			pixels[center_x - y, center_y + x] = (0, 0, 0)
-			pixels[center_x - x, center_y - y] = (0, 0, 0)
-			pixels[center_x - y, center_y - x] = (0, 0, 0)
-			pixels[center_x + x, center_y - y] = (0, 0, 0)
-			pixels[center_x + y, center_y - x] = (0, 0, 0)
+		while x >= y - 1:
+			pixels[center_x + x, center_y + y] = (0, 0, 0, 100)
+			pixels[center_x + y, center_y + x] = (0, 0, 0, 100)
+			pixels[center_x - x, center_y + y] = (0, 0, 0, 100)
+			pixels[center_x - y, center_y + x] = (0, 0, 0, 100)
+			pixels[center_x - x, center_y - y] = (0, 0, 0, 100)
+			pixels[center_x - y, center_y - x] = (0, 0, 0, 100)
+			pixels[center_x + x, center_y - y] = (0, 0, 0, 100)
+			pixels[center_x + y, center_y - x] = (0, 0, 0, 100)
 			
 			y += 1
 			
@@ -243,8 +251,26 @@ class ImageProcessor():
 				x -= 1
 				radius_error += 2 * (y - x + 1)
 			
+	def make_background_transparent(self, cropped_coin):
+		'''transparent_area = (10,20,30,40)
+		mask=Image.new('L', self.image.size, color=255)
+		draw=ImageDraw.Draw(mask) 
+		draw.rectangle(transparent_area, fill=0)
+		self.image.putalpha(mask)
+		self.image.save('output.png')
+		'''
+		
+		size = (self.right - self.left, self.right - self.left)
+		mask = Image.new('L', size, 0)
+		draw = ImageDraw.Draw(mask) 
+		draw.ellipse((0, 0) + size, fill=255)
+		output = ImageOps.fit(cropped_coin, mask.size, centering=(0.5, 0.5))
+		output.putalpha(mask)
+		output.save('output.png')
 			
-			
+	def find_edges(self):
+		self.edge_image = self.image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+		self.edge_image.show()
 			
 			
 			
--- image_processing/process_images.py
+++ image_processing/process_images.py
@@ -1,7 +1,8 @@
 from image_processor import ImageProcessor
 
 test_image = ImageProcessor("images/test_image.jpg")
-test_image.crop()
+test_image.make_coin_image()
+#test_image.find_edges()
 #region = test_image.crop()
 #region.show()
 
